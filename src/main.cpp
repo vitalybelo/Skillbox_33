15f5ca62c9539941a623e7a4fe2510849287b3e4
Vitaly <vitaly.belo@gmail.com>
2023-01-30 09:59:00 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+
+
+    std::cout << "Приветик!" << std::endl;
+
+
+    return 0;
+}
