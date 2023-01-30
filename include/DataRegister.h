e6e4c29e04370176cf6f8ad553258c70f4a5ad4b
Vitaly <vitaly.belo@gmail.com>
2023-01-30 18:19:48 +0300

    check_out3

---
+++ include/DataRegister.h
@@ -0,0 +1,88 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+template<typename Tk, typename Tv>
+class DataRegister {
+private:
+    std::vector<std::pair<Tk,Tv>> data;
+
+public:
+    DataRegister() = default;
+
+    void add();
+    void list();
+    void find();
+    void remove();
+
+};
+
+template<typename Tk, typename Tv>
+void DataRegister<Tk, Tv>::remove() {
+
+    Tk key;
+    std::cout << "\tВведите значение ключа: ";
+    std::cin >> key;
+
+    int result = 0;
+    typename std::vector<std::pair<Tk, Tv>>::iterator iter = data.begin();
+    while(iter != data.end()) {
+        if (iter->first == key) {
+            data.erase(iter);
+            ++result;
+        } else {
+            ++iter;
+        }
+    }
+    std::cout << "\t\tКоличество найденных и удаленных записей = " << result << std::endl;
+
+}
+
+template<typename Tk, typename Tv>
+void DataRegister<Tk, Tv>::find() {
+
+    Tk key;
+    std::cout << "\tВведите значение ключа: ";
+    std::cin >> key;
+
+    int i = 1;
+    bool found_flag = false;
+    std::cout << "\n\tДля ключа: (" << key;
+    for(std::pair<Tk, Tv> p : data) {
+        if (p.first == key) {
+            if (!found_flag) {
+                found_flag = true;
+                std::cout << ") найдено в реестре:\n";
+            }
+            std::cout << "\t\t№" << i++ << ". ключ: " << p.first << " :: значение = " << p.second << std::endl;
+        }
+    }
+    if (!found_flag)
+        std::cout << ") ничего не найдено в реестре:\n";
+}
+
+template<typename Tk, typename Tv>
+void DataRegister<Tk, Tv>::add() {
+
+    std::pair<Tk, Tv> p;
+    std::cout << "\tВведите значение ключа: ";
+    std::cin >> p.first;
+    std::cout << "\tВведите значение для этого ключа: ";
+    std::cin >> p.second;
+
+    data.push_back(p);
+}
+
+template<typename Tk, typename Tv>
+void DataRegister<Tk, Tv>::list() {
+
+    if (!data.empty()) {
+        int i = 1;
+        std::cout << "\nСодержание реестра:\n";
+        for (std::pair<Tk, Tv> p: data) {
+            std::cout << "\t№" << i++ << ". ключ: " << p.first << " :: значение = " << p.second << std::endl;
+        }
+    } else {
+        std::cout << "\nРеестр пустой, записей нет.\n";
+    }
+}
--- src/solution_3.cpp
+++ src/solution_3.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include "use_lib.h"
+#include "DataRegister.h"
 using namespace std;
 
 void solution_3() {
 
-    cout << "\n********************** Решение №3 *********************\n";
+    cout << "\n********************** Решение №3 *********************";
+    cout << "\n:  доступные команды:                                 :";
+    cout << "\n:  add: добавить запись                               :";
+    cout << "\n:  print: список всех записей реестра (list)          :";
+    cout << "\n:  remove: удаление всех записи с заданным ключом     :";
+    cout << "\n:  find: поиск всех записи с заданным ключом          :";
+    cout << "\n:  exit: выход                                        :";
+    cout << "\n*******************************************************\n";
 
+    DataRegister<int, int> data;
 
+    string command;
+    do {
+        clearStdin();
+        cout << "\nВведите команду: ";
+        command = getCommand();
+        if (command == "add") {
+            data.add();
+        } else if (command == "list" || command == "print") {
+            data.list();
+        } else if (command == "find") {
+            data.find();
+        } else if (command == "remove") {
+            data.remove();
+        }
+    } while(command != "exit");
 
 }
